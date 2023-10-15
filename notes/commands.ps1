6bef5a67dc01c80930408b452865f1fc72599c33
Justin Crosby <justin.crosby@gmail.com>
2023-10-15 19:26:17 -0400

    quick commit after using django startproject

---
+++ notes/commands.ps1
@@ -0,0 +1,9 @@
+python3 -m venv .venv #creating virtual environment for the site
+
+
+.\.venv\scripts\activate #to activate the virtual environment.  Console becomes:   (.venv) C:\repos\monarch-entry-only\.venv [main ≡ +2 ~0 -0 !]> 
+deactivate #jump out of virtual environment
+
+pip freeze #shows packages installed (respects virutal environment)
+
+pip install django #install django
